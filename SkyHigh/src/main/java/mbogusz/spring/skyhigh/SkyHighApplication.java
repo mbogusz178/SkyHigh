64f4b375ecba0d62fe8fcf1371b111e32bbeaaf7
mbogusz178 <m.bogusz178@hotmail.com>
2023-11-08 11:59:46 +0100

    Configure project

---
+++ SkyHigh/src/main/java/mbogusz/spring/skyhigh/SkyHighApplication.java
@@ -0,0 +1,13 @@
+package mbogusz.spring.skyhigh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyHighApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyHighApplication.class, args);
+	}
+
+}
