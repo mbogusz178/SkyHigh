6569e934a16007485e497d0dcaef656e95a3ffb1
mbogusz178 <m.bogusz178@hotmail.com>
2023-11-28 18:42:29 +0100

    Add flight search capability

--- src/main/java/mbogusz/spring/skyhigh/repository/FlightRepository.java
+++ src/main/java/mbogusz/spring/skyhigh/repository/FlightRepository.java
@@ -1,10 +1,39 @@
 package mbogusz.spring.skyhigh.repository;
 
 import mbogusz.spring.skyhigh.entity.Flight;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Timestamp;
+import java.util.List;
 
 @Repository
 public interface FlightRepository extends JpaRepository<Flight, Long> {
-
+    @Query(nativeQuery = true, value = "SELECT * FROM search_flights(:source, :destination, :departureAfter, :departureBefore, :arrivalAfter, :arrivalBefore, :adultTicketMinPrice, :adultTicketMaxPrice, :childTicketMinPrice, :childTicketMaxPrice)")
+    List<Flight> searchFlights(@Param("source") String source,
+                               @Param("destination") String destination,
+                               @Param("departureAfter") Timestamp departureAfter,
+                               @Param("departureBefore")Timestamp departureBefore,
+                               @Param("arrivalAfter") Timestamp arrivalAfter,
+                               @Param("arrivalBefore") Timestamp arrivalBefore,
+                               @Param("adultTicketMinPrice") Double adultTicketMinPrice,
+                               @Param("adultTicketMaxPrice") Double adultTicketMaxPrice,
+                               @Param("childTicketMinPrice") Double childTicketMinPrice,
+                               @Param("childTicketMaxPrice") Double childTicketMaxPrice);
+    @Query(nativeQuery = true, value = "SELECT * FROM search_flights(:source, :destination, :departureAfter, :departureBefore, :arrivalAfter, :arrivalBefore, :adultTicketMinPrice, :adultTicketMaxPrice, :childTicketMinPrice, :childTicketMaxPrice)")
+    Page<Flight> searchFlightsPaginated(@Param("source") String source,
+                                        @Param("destination") String destination,
+                                        @Param("departureAfter") Timestamp departureAfter,
+                                        @Param("departureBefore")Timestamp departureBefore,
+                                        @Param("arrivalAfter") Timestamp arrivalAfter,
+                                        @Param("arrivalBefore") Timestamp arrivalBefore,
+                                        @Param("adultTicketMinPrice") Double adultTicketMinPrice,
+                                        @Param("adultTicketMaxPrice") Double adultTicketMaxPrice,
+                                        @Param("childTicketMinPrice") Double childTicketMinPrice,
+                                        @Param("childTicketMaxPrice") Double childTicketMaxPrice,
+                                        Pageable pageable);
 }
